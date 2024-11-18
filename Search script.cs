62b143e3143b8b66294a5255f18da7ef59df0337
vatz <hectorclippedyou@gmail.com>
2024-11-18 07:42:49 -0500

    Create Search script.cs

---
+++ Search script.cs
@@ -0,0 +1,10 @@
+private Task SearchScripts(string query)
+{
+	ScriptHub.<SearchScripts>d__6 <SearchScripts>d__;
+	<SearchScripts>d__.<>t__builder = AsyncTaskMethodBuilder.Create();
+	<SearchScripts>d__.<>4__this = this;
+	<SearchScripts>d__.query = query;
+	<SearchScripts>d__.<>1__state = -1;
+	<SearchScripts>d__.<>t__builder.Start<ScriptHub.<SearchScripts>d__6>(ref <SearchScripts>d__);
+	return <SearchScripts>d__.<>t__builder.Task;
+}
