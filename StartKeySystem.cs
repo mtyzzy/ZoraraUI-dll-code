5ca391746fd9386bab2bf51d9c3af0a79de3f194
vatz <hectorclippedyou@gmail.com>
2024-11-18 18:38:13 -0500

    Create StartKeySystem.cs

---
+++ StartKeySystem.cs
@@ -0,0 +1,8 @@
+private Task FirstStartKeySystem()
+{
+	ClientsWindow.<FirstStartKeySystem>d__26 <FirstStartKeySystem>d__;
+	<FirstStartKeySystem>d__.<>t__builder = AsyncTaskMethodBuilder.Create();
+	<FirstStartKeySystem>d__.<>1__state = -1;
+	<FirstStartKeySystem>d__.<>t__builder.Start<ClientsWindow.<FirstStartKeySystem>d__26>(ref <FirstStartKeySystem>d__);
+	return <FirstStartKeySystem>d__.<>t__builder.Task;
+}
